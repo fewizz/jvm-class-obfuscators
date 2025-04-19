c46665f707ae65edae1f9378508e8dbf0320dce3
fewizz <hazeevaidar@gmail.com>
2025-04-20 00:14:14 +0300

    it does something!

--- src/main/java/ru/fewizz/obfuscators/LexicalObfuscator.java
+++ src/main/java/ru/fewizz/obfuscators/LexicalObfuscator.java
@@ -7,18 +7,23 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
+import org.apache.bcel.classfile.Attribute;
 import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.ConstantClass;
 import org.apache.bcel.classfile.ConstantFieldref;
 import org.apache.bcel.classfile.ConstantLong;
 import org.apache.bcel.classfile.ConstantMethodType;
+import org.apache.bcel.classfile.ConstantMethodref;
 import org.apache.bcel.classfile.ConstantNameAndType;
 import org.apache.bcel.classfile.ConstantString;
 import org.apache.bcel.classfile.ConstantUtf8;
 import org.apache.bcel.classfile.Field;
 import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.LocalVariable;
+import org.apache.bcel.classfile.LocalVariableTable;
 import org.apache.bcel.classfile.Method;
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -36,7 +41,6 @@ public class LexicalObfuscator extends Obfuscator implements Opcodes {
     ) {}
 
     private final Map<String, JavaClass> javaClasses = new HashMap<>();
-    private final Map<JavaClass, byte[]> results = new HashMap<>();
     private final Map<JavaClass, ClassMapping> mappings = new HashMap<>();
 
     private String generateObfuscatedName() {
@@ -51,26 +55,20 @@ public class LexicalObfuscator extends Obfuscator implements Opcodes {
 
         this.javaClasses.put(javaClass.getClassName().replace('.', '/'), javaClass);
 
-        return () -> this.results.get(javaClass);
+        return () -> {
+            try {
+                var outputStream = new ByteArrayOutputStream();
+                this.obfuscate(javaClass).dump(outputStream);
+                return outputStream.toByteArray();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 
     public void end() throws Exception {
-        // Задать маппинги
         for (JavaClass javaClass : this.javaClasses.values()) {
             this.createMappings(javaClass);
-        }
-
-        // Применить маппинги
-        for (JavaClass javaClass : this.javaClasses.values()) {
-            this.obfuscate(javaClass);
-        }
-
-        // Конвертировать обратно в байт-код
-        for (JavaClass javaClass : this.javaClasses.values()) {
-            var outputStream = new ByteArrayOutputStream();
-            javaClass.dump(outputStream);
-            byte[] bytes = outputStream.toByteArray();
-            this.results.put(javaClass, bytes);
         }
     }
 
@@ -96,57 +94,119 @@ public class LexicalObfuscator extends Obfuscator implements Opcodes {
         }
     }
 
-    private void obfuscate(JavaClass javaClass) throws Exception {
-        var pool = javaClass.getConstantPool();
+    private JavaClass obfuscate(JavaClass srcJavaClass) throws Exception {
+        ClassMapping cm = this.mappings.get(srcJavaClass);
+        JavaClass dstJavaClass = srcJavaClass.copy();
 
-        ClassMapping thisCm = this.mappings.get(javaClass);
+        var srcPool = srcJavaClass.getConstantPool();
+        var dstPool = dstJavaClass.getConstantPool();
 
-        for (int i = 1; i < pool.getLength(); ++i) {
-            var constant = pool.getConstant(i);
+        Function<String, Integer> getUTF8Index = (String name) -> {
+            for (int i = 1; i < dstPool.getLength(); ++i) {
+                var c = dstPool.getConstant(i);
+                if (c instanceof ConstantLong) { ++i; continue; }
+                if (c instanceof ConstantUtf8 u && u.getBytes().equals(name)) { return i; }
+            }
+            var newConstant = new ConstantUtf8(name);
+            var newNameIndex = dstPool.getLength();
+            var newPoolArray = Arrays.copyOf(dstPool.getConstantPool(), newNameIndex+1);
+            newPoolArray[newNameIndex] = newConstant;
+            dstPool.setConstantPool(newPoolArray);
+            return newNameIndex;
+        };
+
+        // Fields
+        for (int i = 1; i < srcPool.getLength(); ++i) {
+            var constant = srcPool.getConstant(i);
             if (constant instanceof ConstantLong) { ++i; continue; }
-            if (!(constant instanceof ConstantFieldref cfr)) { continue;}
+            if (!(constant instanceof ConstantFieldref srcCFR)) { continue;}
 
-            String ownerName = cfr.getClass(pool).replace('.', '/');
+            JavaClass owner = this.javaClasses.get(srcCFR.getClass(srcPool).replace('.', '/'));
+            if (owner == null) { continue; }
+            ConstantNameAndType srcCNT = srcPool.getConstant(srcCFR.getNameAndTypeIndex());
+            Field field = findField(owner, srcCNT.getName(srcPool), srcCNT.getSignature(srcPool));
+            var fieldMapping = this.mappings.get(owner).fieldMappings.get(field);
+            if (fieldMapping == null) { continue; }
+
+            ConstantFieldref dstCFR = dstPool.getConstant(i);
+            ConstantNameAndType dstCNT = dstPool.getConstant(dstCFR.getNameAndTypeIndex());
+            dstCNT.setNameIndex(getUTF8Index.apply(fieldMapping));
+            dstCNT.setSignatureIndex(getUTF8Index.apply(this.patchDescriptor(srcCNT.getSignature(srcPool))));
+        }
+        for (int i = 0; i < srcJavaClass.getFields().length; ++i) {
+            var translated = cm.fieldMappings.get(srcJavaClass.getFields()[i]);
+            if (translated == null) { continue; }
+            dstJavaClass.getFields()[i].setNameIndex(getUTF8Index.apply(translated));
+        }
+
+        // Methods
+        for (int i = 1; i < srcPool.getLength(); ++i) {
+            var constant = srcPool.getConstant(i);
+            if (constant instanceof ConstantLong) { ++i; continue; }
+            if (!(constant instanceof ConstantMethodref srcCMR)) { continue;}
+
+            JavaClass owner = this.javaClasses.get(srcCMR.getClass(srcPool).replace('.', '/'));
+            if (owner == null) { continue; }
+            ConstantNameAndType srcCNT = srcPool.getConstant(srcCMR.getNameAndTypeIndex());
+            Method method = findMethod(owner, srcCNT.getName(srcPool), srcCNT.getSignature(srcPool));
+            var methodMapping = this.mappings.get(owner).methodMappings.get(method);
+            if (methodMapping == null) { continue; }
+
+            ConstantMethodref dstCMR = dstPool.getConstant(i);
+            ConstantNameAndType dstCNT = dstPool.getConstant(dstCMR.getNameAndTypeIndex());
+            dstCNT.setNameIndex(getUTF8Index.apply(methodMapping));
+            dstCNT.setSignatureIndex(getUTF8Index.apply(this.patchDescriptor(srcCNT.getSignature(srcPool))));
+        }
+        for (int i = 0; i < srcJavaClass.getMethods().length; ++i) {
+            Method srcMethod = srcJavaClass.getMethods()[i];
+            Method dstMethod = dstJavaClass.getMethods()[i];
+
+            for (int x = 0; x < srcMethod.getAttributes().length; ++x) {
+                var a = srcMethod.getAttributes()[x];
+                if (!(a instanceof LocalVariableTable lvt)) { continue; }
+                for (int y = 0; y < lvt.getTableLength(); ++y) {
+                    LocalVariable lv = lvt.getLocalVariableTable()[y];
+                    int dstDescriptorIndex = getUTF8Index.apply(this.patchDescriptor(lv.getSignature()));
+                    ((LocalVariableTable)dstMethod.getAttributes()[x]).getLocalVariableTable()[y].setSignatureIndex(dstDescriptorIndex);
+                }
+            }
+
+            var translated = cm.methodMappings.get(srcMethod);
+            if (translated == null) { continue; }
+            dstMethod.setNameIndex(getUTF8Index.apply(translated));
+        }
+
+        // Others
+        for (int i = 1; i < srcPool.getLength(); ++i) {
+            var constant = srcPool.getConstant(i);
+            if (constant instanceof ConstantLong) { ++i; continue; }
+            if (!(constant instanceof ConstantClass srcCC)) { continue;}
+
+            String ownerName = srcPool.getConstantUtf8(srcCC.getNameIndex()).getBytes().replace('.', '/');
             JavaClass owner = this.javaClasses.get(ownerName);
             if (owner == null) { continue; }
-
-            ClassMapping cm = this.mappings.get(owner);
-            ConstantNameAndType cnt = pool.getConstant(cfr.getNameAndTypeIndex());
-
-            Field field = findField(owner, cnt.getName(pool), cnt.getSignature(pool));
-
-            var fieldMapping = cm.fieldMappings.get(field);
-            if (fieldMapping == null) { continue; }
-
-            var newName = new ConstantUtf8(fieldMapping);
-            var newNameIndex = pool.getLength();
-
-            var newPoolArray = Arrays.copyOf(pool.getConstantPool(), newNameIndex+1);
-            newPoolArray[newNameIndex] = newName;
-            pool.setConstantPool(newPoolArray);
-
-            cnt.setNameIndex(newNameIndex);
+            ConstantClass dstCFR = dstPool.getConstant(i);
+            String translatedName = this.mappings.get(owner).translated;
+            System.out.println(ownerName+" -> "+translatedName);
+            dstCFR.setNameIndex(getUTF8Index.apply(translatedName));
         }
 
-        for (Field field : javaClass.getFields()) {
-            var fieldMapping = thisCm.fieldMappings.get(field);
-            if (fieldMapping == null) { continue; }
-
-            var newName = new ConstantUtf8(fieldMapping);
-            var newNameIndex = pool.getLength();
-
-            var newPoolArray = Arrays.copyOf(pool.getConstantPool(), newNameIndex+1);
-            newPoolArray[newNameIndex] = newName;
-            pool.setConstantPool(newPoolArray);
-
-            field.setNameIndex(newNameIndex);
-        }
+        return dstJavaClass;
     }
 
     private Field findField(JavaClass javaClass, String name, String descriptor) {
         for (Field f : javaClass.getFields()) {
             if (f.getName().equals(name) && f.getSignature().equals(descriptor)) {
                 return f;
+            }
+        }
+        return null;
+    }
+
+    private Method findMethod(JavaClass javaClass, String name, String descriptor) {
+        for (Method m : javaClass.getMethods()) {
+            if (m.getName().equals(name) && m.getSignature().equals(descriptor)) {
+                return m;
             }
         }
         return null;
