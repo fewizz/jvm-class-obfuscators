22fb30c3a2ccc285611c333cc691b52f45619967
fewizz <hazeevaidar@gmail.com>
2025-06-02 22:53:01 +0300

    allow stacking obfuscators

--- src/main/java/ru/fewizz/Main.java
+++ src/main/java/ru/fewizz/Main.java
@@ -2,93 +2,96 @@ package ru.fewizz;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Supplier;
+import java.util.logging.Logger;
 
 import org.apache.bcel.classfile.ClassParser;
 
-import ru.fewizz.obfuscators.ControlFlowObfuscator;
-import ru.fewizz.obfuscators.DebugMetadataDeleter;
-import ru.fewizz.obfuscators.InvokeDynamicStringConstantsObfuscator;
-import ru.fewizz.obfuscators.LexicalObfuscator;
-import ru.fewizz.obfuscators.NaiveStringConstantsObfuscator;
-
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger("obfuscator");
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
-        String type = args[0];
-        args = Arrays.copyOfRange(args, 1, args.length);
-
-        Obfuscator obfuscator = switch (type) {
-            case "control-flow" -> new ControlFlowObfuscator();
-            case "string-constants-naive" -> new NaiveStringConstantsObfuscator();
-            case "string-constants-invoke-dynamic" -> new InvokeDynamicStringConstantsObfuscator();
-            case "lexical" -> new LexicalObfuscator();
-            case "debug" -> new DebugMetadataDeleter();
-            default -> throw new RuntimeException("Unknown obfuscator: \"" + type + "\"");
-        };
-
         Path src = Paths.get(args[0]);
         Path dst = Paths.get(args[1]);
 
-        List<Supplier<byte[]>> suppliers = new ArrayList<>();
+        LOGGER.info("loading obfuscators classes");
+        List<Class<Obfuscator>> obfuscatorClasses = new ArrayList<>();
+        Iterable<URL> iter = () -> Main.class.getClassLoader().resources("META-INF/obfuscators").iterator();
+        for (var url : iter) {
+            try(Scanner scan = new Scanner(url.openStream())) {
+                while (scan.hasNextLine()) {
+                    var line = scan.nextLine();
+                    obfuscatorClasses.add((Class<Obfuscator>) Class.forName(line));
+                }
+            }
+        }
+
+        LOGGER.info("creating selected obfuscators");
+        List<Obfuscator> obfuscators = new ArrayList<>();
+        for (var obfuscatorName : Arrays.copyOfRange(args, 2, args.length)) {
+            Class<Obfuscator> obfuscatorClass = obfuscatorClasses.stream()
+                .filter(c -> c.getName().equals(obfuscatorName))
+                .findFirst().get();
+            obfuscators.add(obfuscatorClass.getConstructor().newInstance());
+        }
+
+        LOGGER.info("loading class files");
+        List<byte[]> classesBytes = new ArrayList<>();
 
         // Если на вход подается путь до файла,
         // то обрабатывается только один файл
         if (!Files.isDirectory(src)) {
-            suppliers.add(handleFile(src, dst, obfuscator));
+            classesBytes.add(Files.readAllBytes(src));
         }
         // Рекурсивно обрабатываются все файлы в исходной директории
         else {
             Files.walk(src).forEach(srcFile -> {
-                if (Files.isDirectory(srcFile))
-                    return;
-                suppliers.add(handleFile(srcFile, dst, obfuscator));
+                if (Files.isDirectory(srcFile)) return;
+                try {
+                    classesBytes.add(Files.readAllBytes(srcFile));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             });
         }
 
-        obfuscator.onAllClassesProvided();
+        for (var obfuscator : obfuscators) {
+            LOGGER.info("obfuscating class files with " + obfuscator.getClass().getName());
+
+            List<Supplier<byte[]>> obfuscatedClassBytesSuppliers = new ArrayList<>();
+            for (var classFile : classesBytes) {
+                obfuscatedClassBytesSuppliers.add(obfuscator.getObfuscatedClassSupplier(classFile));
+            }
+            obfuscator.onAllClassesProvided();
+            classesBytes.clear();
+            for (var supplier : obfuscatedClassBytesSuppliers) {
+                classesBytes.add(supplier.get());
+            }
+        }
 
         // 3. Запись байтов класс-файла в файл назначения
-        for (var s : suppliers) {
-            byte[] transformedClassBytes = s.get();
+        LOGGER.info("writing obfuscated classes");
+        for (var classBytes : classesBytes) {
             try {
                 // Костыль - парсим класс еще раз, чтобы получить (вероятно) обфусцированное имя класса
-                var inputStream = new ByteArrayInputStream(transformedClassBytes);
+                var inputStream = new ByteArrayInputStream(classBytes);
                 var parser = new ClassParser(inputStream, "");
                 var javaClass = parser.parse();
                 var dstPath = dst.resolve(javaClass.getClassName().replace('.', '/').concat(".class"));
                 Files.createDirectories(dstPath.getParent());
-                Files.write(dstPath, transformedClassBytes);
+                Files.write(dstPath, classBytes);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }
-    }
-
-    private static Supplier<byte[]> handleFile(
-        Path srcFile,
-        Path dst,
-        Obfuscator obfuscator
-    ) {
-         try {
-            // Создание директории назначения
-            if (dst.getParent() != null) {
-                Files.createDirectories(dst.getParent());
-            }
-
-            // 1. Чтение байтов исходного класс-файла
-            byte[] classFileBytes = Files.readAllBytes(srcFile);
-
-            // 2. Планирование обработки класса
-            return obfuscator.getObfuscatedClassSupplier(classFileBytes);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
